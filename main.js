3a70f6a51fe56e066f0c3de4c575b3cfb6721850
lukegarst <105182557+lukegarst@users.noreply.github.com>
2022-06-21 19:46:51 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+var menu = {
+    courses: [
+        {
+        'appetizers': [],
+        'mains': [],
+        'desserts': []
+    }
+],
+
+    addDishToCourse: [
+        function (courseName, dishName, dishPrice) {
+        var dish = [
+            {'name': '',
+            'price': ''
+        }
+        ];
+    }
+],
+};
